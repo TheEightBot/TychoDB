3c2ea9bb652d97444016b42ba6041d9d2702d594
Michael Stonis <michael.stonis@eightbot.com>
2025-10-27 16:42:41 -0500

    Removes unnecessary optimization pragma
    Removes the explicit optimization pragma.
    
    *   The auto_vacuum pragma already provides incremental optimization.
    *   Removing the explicit `PRAGMA optimize` avoids redundant operations and potential performance overhead.

--- TychoDB/Queries.cs
+++ TychoDB/Queries.cs
@@ -14,7 +14,6 @@ internal static class Queries
         PRAGMA synchronous = NORMAL;
         PRAGMA locking_mode = EXCLUSIVE;
         PRAGMA auto_vacuum = INCREMENTAL;
-        PRAGMA optimize;
 
         CREATE TABLE IF NOT EXISTS JsonValue
         (
